17db48dd2cac2a3a8890af8b1f67bb9b50f4fd18
Abdul Majeed <majeedkhan.ub1@gmail.com>
2026-06-24 18:53:39 +0500

    fixed tenant identifier in supabase connection

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'aws-0-ap-southeast-1.pooler.supabase.com';
 $db   = 'postgres';
-$user = 'postgres';
+$user = 'postgres.xzkeuffylzineoddnbb';
 $pass = 'LaravelLivewire12@'; // ⚠️ Agar aapne koi aur password rakha tha toh woh likhein
 $port = '5432';
 
