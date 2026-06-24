17d63a895a9898918c371eccbaeaff9c3e8e2e95
Abdul Majeed <majeedkhan.ub1@gmail.com>
2026-06-24 18:45:49 +0500

    database setup complete with supabase

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,14 @@
 <?php
+$host = 'aws-0-ap-southeast-1.pooler.supabase.com';
+$db   = 'postgres';
+$user = 'postgres';
+$pass = 'LaravelLivewire12@'; // ⚠️ Agar aapne koi aur password rakha tha toh woh likhein
+$port = '5432';
 
-$con=mysqli_connect('localhost','root','','mystore');
-
-
-if (!$con) {
-    die("Database connection failed: " . mysqli_connect_error());
+try {
+    $con = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
 }
-
 ?>
